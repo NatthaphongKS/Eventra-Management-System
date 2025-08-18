faad0b93d0337dbc252de45bcd0683424d4e076d
Chitdanai <gamerzfire2215@gmail.com>
2025-08-18 21:23:57 +0700

    upgrade : create event

--- ems/vite.config.js
+++ ems/vite.config.js
@@ -1,11 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
-    laravel({ input: ['resources/js/app.js'], refresh: true }),
+    laravel({
+      input: ['resources/js/app.js'],   // ถ้ามี css แยกก็ใส่เพิ่ม
+      refresh: true,
+    }),
     vue(),
   ],
 })
