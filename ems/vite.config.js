67a4eeb571376bbbaf90974fe8f640755768b240
NatthaphongKS <Natthaphong.KS1@gmail.com>
2025-08-19 17:51:30 +0700

    docker

--- ems/vite.config.js
+++ ems/vite.config.js
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//   plugins: [
+//     laravel({
+//       input: ['resources/js/app.js'],
+//       refresh: true,
+//     }),
+//     vue(),
+//   ],
+// });
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
   plugins: [
+    vue(),
     laravel({
-      input: ['resources/js/app.js'],
+      input: ['resources/js/app.js', 'resources/css/app.css'],
       refresh: true,
     }),
-    vue(),
   ],
-});
+  server: {
+    host: true,         // ให้เข้าถึงจากภายนอกคอนเทนเนอร์
+    port: 5173,
+    hmr: {
+      host: 'localhost' // ให้ HMR ชี้กลับมาที่เครื่องเรา
+    }
+  }
+})
