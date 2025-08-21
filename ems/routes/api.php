9eb9209e991f5db2f0b786fa7c2fae43ff336487
Chitdanai <gamerzfire2215@gmail.com>
2025-08-21 20:30:58 +0700

    Update api.php

--- ems/routes/api.php
+++ ems/routes/api.php
@@ -12,10 +12,12 @@ Route::middleware(['web','auth'])->group(function () {
     Route::get('/get-employees', [EmployeeController::class, 'index']);
     Route::get('/event-info', [EventController::class, 'eventInfo']);
     Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+    Route::get('/event/{evn_id}/employee/{emp_id}', [EmployeeController::class, 'show']);
 
     Route::post('/save-employee', [EmployeeController::class, 'store']);
     Route::post('/save-department', [EmployeeController::class, 'saveDepartment']);
     Route::post('/save-position', [EmployeeController::class, 'savePosition']);
     Route::post('/save-team', [EmployeeController::class, 'saveTeam']);
     Route::post('/event-save', [EventController::class, 'store']);
+    
 });
