bfd6f615ada5f1cd356f5b92c73eaf6f07f75617
Nattapong Kamma <Icezazarun@gmail.com>
2025-08-22 21:34:22 +0700

    NattapongKM

--- ems/routes/api.php
+++ ems/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\CategoryController;
+
 
 // ตัวอย่าง API ที่ต้อง login (คุณมีอะไรใช้ก็ครอบไว้ได้)
 Route::middleware(['web','auth'])->group(function () {
@@ -12,10 +15,15 @@ Route::middleware(['web','auth'])->group(function () {
     Route::get('/get-employees', [EmployeeController::class, 'index']);
     Route::get('/event-info', [EventController::class, 'eventInfo']);
     Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
 
     Route::post('/save-employee', [EmployeeController::class, 'store']);
     Route::post('/save-department', [EmployeeController::class, 'saveDepartment']);
     Route::post('/save-position', [EmployeeController::class, 'savePosition']);
     Route::post('/save-team', [EmployeeController::class, 'saveTeam']);
     Route::post('/event-save', [EventController::class, 'store']);
+Route::post('/categories', [CategoryController::class, 'store']);
+
 });
