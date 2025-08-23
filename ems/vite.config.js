dc4426d10f8a2e3d794010dd42e255a0b026b172
NatthaphongKS <Natthaphong.KS1@gmail.com>
2025-08-23 09:05:49 +0700

    docker app server

--- ems/vite.config.js
+++ ems/vite.config.js
@@ -1,13 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
-    laravel({
-      input: ['resources/js/app.js'],   // ถ้ามี css แยกก็ใส่เพิ่ม
-      refresh: true,
-    }),
+    laravel({ input: ['resources/js/app.js'], refresh: true }),
     vue(),
   ],
+  server: {
+    host: '0.0.0.0',          // ให้ container เปิดพอร์ตออกมาได้
+    port: 7173,
+    strictPort: true,
+    hmr: { host: '10.80.7.17', port: 7173 }, // ให้เบราว์เซอร์ยิงกลับ localhost
+  },
 })
