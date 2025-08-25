669624d0abaaf87b36b8c1b64507b40fb1b84587
MeowCAT378 <yothin0637354579@gmail.com>
2025-08-25 20:04:04 +0700

    Fix Event table

--- ems/routes/api.php
+++ ems/routes/api.php
@@ -1,23 +1,29 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\EventController;
 
-// ตัวอย่าง API ที่ต้อง login (คุณมีอะไรใช้ก็ครอบไว้ได้)
-Route::middleware(['web','auth'])->group(function () {
+// ถ้า "ทุกหน้า" ต้องล็อกอิน คงไว้ใน group เดิมก็ได้
+Route::middleware(['web', 'auth'])->group(function () {
     Route::post('/logout', [LoginController::class, 'logout']);
+
+    // === Employee ===
     Route::get('/meta', [EmployeeController::class, 'meta']);
     Route::get('/get-employees', [EmployeeController::class, 'index']);
-    Route::get('/event-info', [EventController::class, 'eventInfo']);
     Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
-    Route::get('/event/{evn_id}/employee/{emp_id}', [EmployeeController::class, 'show']);
-
     Route::post('/save-employee', [EmployeeController::class, 'store']);
     Route::post('/save-department', [EmployeeController::class, 'saveDepartment']);
     Route::post('/save-position', [EmployeeController::class, 'savePosition']);
     Route::post('/save-team', [EmployeeController::class, 'saveTeam']);
+
+    // === Event ===
+    Route::get('/event-info', [EventController::class, 'eventInfo']);
+    Route::get('/get-event', [EventController::class, 'Eventtable']);   // << ใช้กับหน้า List
+    Route::delete('/event/{id}', [EventController::class, 'destroy']);  // << ปุ่มลบในหน้า Vue
     Route::post('/event-save', [EventController::class, 'store']);
-    
+    Route::get('/event/{evn_id}/employee/{emp_id}', [EmployeeController::class, 'show']);
+
+    // (ถ้าคุณยังมี index() อยู่และใช้งาน ก็เก็บไว้)
+    Route::get('/event', [EventController::class, 'index']);
 });
