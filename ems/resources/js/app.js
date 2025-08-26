264c3440651fb713de67591fc8293ae62a2f5ee8
MiiShot <kidrakon838084@gmail.com>
2025-08-26 17:09:36 +0700

    test

--- ems/resources/js/app.js
+++ ems/resources/js/app.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue';
-import '../css/app.css'
+import '../css/app.css';
 import App from './App.vue';
-import axios from 'axios'
-axios.defaults.withCredentials = true
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
-axios.defaults.headers.common['X-CSRF-TOKEN'] =
-  document.querySelector('meta[name="csrf-token"]')?.content
+import router from './router'; // เพิ่มการเชื่อมต่อ Vue Router
+import axios from 'axios';
 
-createApp(App).mount('#app');
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['X-CSRF-TOKEN'] =
+  document.querySelector('meta[name="csrf-token"]')?.content;
+
+const app = createApp(App);
+app.use(router); // ใช้ Vue Router
+app.mount('#app');
