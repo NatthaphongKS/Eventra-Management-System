f5110beac9ef1990520eba53c4271fa8eb9ca747
MiiShot <kidrakon838084@gmail.com>
2025-08-27 10:15:05 +0700

    test01

--- ems/resources/views/spa.blade.php
+++ ems/resources/views/spa.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/main.js'])
     <title>Laravel Vue SPA</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite('resources/js/main.js')
+    @vite('resources/js/app.js')
 </head>
 <body>
     <div id="app"></div>
