f155319c88a295bedb17862ba2229da460362d7c
Nitiwadee-Pang <pang.nitiwadee@gmail.com>
2025-08-27 16:40:31 +0700

    refactor: update EventController and HistoryEventController for improved event data handling; enhance History_Employees_page with search and pagination features

--- ems/app/Http/Controllers/HistoryEventController.php
+++ ems/app/Http/Controllers/HistoryEventController.php
@@ -8,28 +8,14 @@ use App\Models\Position;
 use App\Models\Department;
 use App\Models\Team;
 use App\Models\Category;
+use App\Models\Event;
 
 class HistoryEventController extends Controller
 {
-     public function eventInfo()
+    public function eventInfo()
     {
-        $employees = Employee::join('ems_position', 'ems_employees.emp_position_id', '=', 'ems_position.id')
-            ->join('ems_department', 'ems_employees.emp_department_id', '=', 'ems_department.id')
-            ->join('ems_team', 'ems_employees.emp_team_id', '=', 'ems_team.id')
-            ->select(
-                'ems_employees.*',
-                'ems_position.pst_name as position_name',
-                'ems_department.dpm_name as department_name',
-                'ems_team.tm_name as team_name'
-            )
-            ->where('ems_employees.emp_delete_status', 'inactive')
-            ->get();
-
-        $categories  = Category::select('id','cat_name')->where('cat_delete_status','active')->orderBy('cat_name')->get();
-        $positions   = Position::select('id','pst_name')->where('pst_delete_status','active')->orderBy('pst_name')->get();
-        $departments = Department::select('id','dpm_name')->where('dpm_delete_status','active')->orderBy('dpm_name')->get();
-        $teams       = Team::select('id','tm_name')->where('tm_delete_status','active')->orderBy('tm_name')->get();
-
-        return response()->json(compact('categories','employees','positions','departments','teams'));
+        $events = Event::join('ems_categories', 'ems_event.evn_category_id', '=', 'ems_categories.id')
+            ->where('evn_status', 'deleted')->get();
+        return response()->json($events);
     }
 }
