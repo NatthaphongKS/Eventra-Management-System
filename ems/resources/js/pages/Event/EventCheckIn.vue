c4fccc69a020f7f102f788ca98560b2d91780bf7
Raveroj <Raverojsonthi47@gmail.com>
2025-10-18 18:19:05 +0700

    fix() : button add new change to add and add comment in page EventCheckIn

--- ems/resources/js/pages/Event/EventCheckIn.vue
+++ ems/resources/js/pages/Event/EventCheckIn.vue
@@ -12,7 +12,7 @@
 <script setup>
 import { ref } from 'vue'
 import EventCheckInEvent from '@/components/IndexEvent/EventCheckInEvent.vue'
-
+//ข้อมูล mock 
 const employees = ref([
   { id:1, emp_id:'CH0001', emp_prefix:'นาย', emp_firstname:'พศิน', emp_lastname:'สายชล', emp_nickname:'พีท', department_name:'Engineering', team_name:'CodeCraft', position_name:'Software Engineer' },
   { id:2, emp_id:'CH0002', emp_prefix:'น.ส.', emp_firstname:'อรทัย', emp_lastname:'จงรัก', emp_nickname:'อร', department_name:'Product Development', team_name:'DataDynamics', position_name:'Frontend Developer' },
