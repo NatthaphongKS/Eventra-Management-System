586c472309fd1ad24f2f252d53e5fcc09fc76ae4
NatthaphongKS <Natthaphong.KS1@gmail.com>
2025-10-23 14:12:26 +0700

    feat: Check-in Event
    show employee

--- ems/resources/js/pages/Event/EventCheckIn.vue
+++ ems/resources/js/pages/Event/EventCheckIn.vue
@@ -1,24 +1,37 @@
 <template>
-  <EventCheckInEvent
-    :rows="employees"
-    v-model="selectedIds"
-    :page="page"
-    :page-size="pageSize"
-    @update:page="(p) => page = p"
-    @update:page-size="(s) => pageSize = s"
-  />
+  <EventCheckInEvent :rows="rows" v-model="selectedIds" :page="page" :page-size="pageSize"
+    @update:page="(p) => page = p" @update:page-size="(s) => pageSize = s" />
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from 'axios'
 import EventCheckInEvent from '@/components/IndexEvent/EventCheckInEvent.vue'
 //ข้อมูล mock 
-const employees = ref([
-  { id:1, emp_id:'CH0001', emp_prefix:'นาย', emp_firstname:'พศิน', emp_lastname:'สายชล', emp_nickname:'พีท', department_name:'Engineering', team_name:'CodeCraft', position_name:'Software Engineer' },
-  { id:2, emp_id:'CH0002', emp_prefix:'น.ส.', emp_firstname:'อรทัย', emp_lastname:'จงรัก', emp_nickname:'อร', department_name:'Product Development', team_name:'DataDynamics', position_name:'Frontend Developer' },
-])
+
+const route = useRoute()
+const eveId = route.params.id // ปรับตามที่เก็บ eveId
+console.log('Event ID:', eveId)
+const rows = ref([])
+
+async function fetchEmployeeAttendancers() {
+  try {
+    const res = await axios.get(`/api/getEmployeeAttendancers/eveId/${eveId}`)
+    // คืนเป็น array โดยดูจากโครงสร้างจริงของ response
+    console.log('Fetched employee attendances:', res.data)
+    return res.data.data ?? res.data ?? []
+  } catch (err) {
+    console.error('Failed to fetch employee attendances:', err)
+  }
+}
 
 const selectedIds = ref([])
 const page = ref(1)
 const pageSize = ref(10)
+
+// เรียก API เมื่อเข้าหน้า
+onMounted(async () => {
+  rows.value = await fetchEmployeeAttendancers()
+})
 </script>
