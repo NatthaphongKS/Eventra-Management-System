fbd5059af778190bb8e996d0439010e8c9274384
Chitdanai <gamerzfire2215@gmail.com>
2025-10-23 14:56:48 +0700

    Update main.js

--- ems/resources/js/main.js
+++ ems/resources/js/main.js
@@ -1,8 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
 
 import axios from 'axios'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 1. Import router ของคุณ
+// (เช็ก Path ให้ถูกนะครับ บางทีอาจจะเป็น './router/index.js')
+import router from './router' 
+
+const app = createApp(App)
+
+// 2. สั่งให้แอป Vue "ใช้" router (จุดที่มักจะลืม)
+// บรรทัดนี้ต้องอยู่ก่อน app.mount()
+app.use(router) 
 axios.defaults.withCredentials = true // ถ้าใช้ cookie-based auth
+
+// 3. Mount แอป
+app.mount('#app')
