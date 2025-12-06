a78417ba58c385a03f0d1763497ec2931a619257
Nitiwadee-Pang <pang.nitiwadee@gmail.com>
2025-12-06 15:12:16 +0700

    login-UI

--- ems/resources/js/pages/Login.vue
+++ ems/resources/js/pages/Login.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="min-h-screen flex items-center justify-end pr-[8vw] md:pr-[12vw] px-6 bg-red-700">
+  <div class="min-h-screen flex items-center justify-end pr-[8vw]  bg-[url('/images/email/ChatGPT_Image_20_.._2568_23_31_01.png')] bg-cover bg-center md:pr-[12vw] px-6 bg-red-700">
     <div class="rounded-[28px] bg-white shadow-lg p-8 md:p-10 w-[484px] h-[592px]">
-      <div class="text-center text-3xl font-semibold mb-6 text-red-700">Eventra</div>
+      <div class="text-center text-5xl font-semibold mb-6 text-red-700">Eventra</div>
 
 
     <div class="left">
-      <h2 class="text-3xl font-semibold text-gray-900 mb-6">Sign In</h2>
+      <h2 class="mt-16 text-4xl font-semibold text-gray-900 mb-5">Sign In</h2>
       <form @submit.prevent="login">
-        <div class="p-2">
+        <div class="p-2 text-2xl">
           <label></label>
-          <input type="email" v-model="email" class=" w-full border border-red-700 rounded-3xl p-3 px-5 placeholder:text-gray-800 placeholder:font-semibold" placeholder="Email" />
+          <input type="email" v-model="email" class="w-[403px] h-[70px] border border-red-700 rounded-full p-3 px-5 placeholder:text-neutral-600 placeholder:font-semibold focus:outline-none focus:ring-0" placeholder="Email" />
 
         </div>
-        <div class="p-2">
+        <div class="p-2 text-2xl">
           <label></label>
-          <input type="password" v-model="password" class=" w-full border border-red-700 rounded-3xl p-3 px-5 placeholder:text-gray-800 placeholder:font-semibold" placeholder="Password" />
+          <input type="password" v-model="password" class="w-[403px] h-[70px] border border-red-700 rounded-full p-3 px-5 placeholder:text-neutral-600 placeholder:font-semibold focus:outline-none focus:ring-0" placeholder="Password" />
+        </div>
+        <div class="flex justify-center mt-16">
+           <a href="#" class="text-rose-200 text-sm underline text-base font-medium">forgot password</a>
         </div>
         <div class="flex justify-center">
-          <button type="submit" class="w-2/4 bg-red-700 text-white rounded-3xl p-3 mt-4 font-semibold">Login</button>
+          <button type="submit" class="w-[181px] h-[57px] bg-red-700 text-white hover:bg-red-800 rounded-3xl p-3 mt-4 font-semibold text-2xl ">Sign In</button>
         </div>
       </form>
       <p v-if="error">{{ error }}</p>
