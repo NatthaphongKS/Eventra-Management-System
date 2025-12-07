70f5d985097363a9463be6ba4bc58d18dc2c807a
Raveroj <Raverojsonthi47@gmail.com>
2025-12-07 15:05:44 +0700

    add : comment Login and LoginController

--- ems/app/Http/Controllers/LoginController.php
+++ ems/app/Http/Controllers/LoginController.php
@@ -17,6 +17,7 @@ class LoginController extends Controller
     }
     public function login(Request $request)
     {
+        //ส่วน validate ๙่องกรอก email password
         $request->validate([
             'email' => 'required|email',
             'password' => 'required'
@@ -24,13 +25,15 @@ class LoginController extends Controller
     );
 
         $employee = Employee::where('emp_email', $request->email)->first();
+        //ส่วน validate กรณี ใส่ email หรือ password ผิด
         if (!$employee || !Hash::check($request->password, $employee->emp_password)) {
             return response()->json(['message' => 'Incorrect username or password. Please try again'], 404);
         }
+        //ส่วน validate กรณี ใส่ email มีสถานะ disable
         if ($employee->emp_status === 'disabled') {
             return response()->json(['message' => 'Account can not login'], 403);
         }
-
+        //ส่วน validate กรณี ใส่ account นี้โดนลบ แล้วมีสถานะ inactive
         if ($employee->emp_delete_status === 'inactive') {
             return response()->json(['message' => 'Account is inactive'], 403);
         }
