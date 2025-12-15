0fe88d1688acefec675947e01d5dc0509c601b9e
NatthaphongKS <Natthaphong.KS1@gmail.com>
2025-12-15 09:41:04 +0700

    feat : build deployV5

--- ems/vite.config.js
+++ ems/vite.config.js
@@ -1,41 +1,34 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import vue from '@vitejs/plugin-vue'
+
+// --------------------------------------------------------------------
+// server
+// --------------------------------------------------------------------
+
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [
-    laravel({ input: ['resources/js/app.js','resources/js/main.js'], refresh: true }),
-    vue(),
-  ],
-  server: {
-  host: '0.0.0.0',
-  port: 5173,
-  strictPort: true,
-
-  hmr: {
-    host: 'localhost',
-    port: 5173,
-  },
-
-  // ✅ สำคัญมากสำหรับ Mac + Docker (ให้จับการแก้ไฟล์แบบเรียลไทม์)
-  watch: {
-    usePolling: true,
-    interval: 100,
-  },
-
-  // ✅ ถ้าจะ proxy /api ไป Laravel ใน Docker
-  proxy: {
-    '/api': {
-      target: 'http://ems_app_dev', // หรือ 'http://app' ถ้าชื่อ service คือ app
-      changeOrigin: true,
-      secure: false,
+    plugins: [
+        laravel({ input: ["resources/js/app.js"], refresh: true }),
+        vue(),
+    ],
+    server: {
+        host: "0.0.0.0", // ให้ container เปิดพอร์ตออกมาได้
+        port: 7222,
+        strictPort: true,
+        hmr: { host: "10.80.7.17", port: 7222 }, // ให้เบราว์เซอร์ยิงกลับ localhost
+        proxy: {
+            "/api": {
+                target: "http://dekdee2.informatics.buu.ac.th:7111",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/sanctum": {
+                target: "http://dekdee2.informatics.buu.ac.th:7111",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
     },
-    '/sanctum': {
-      target: 'http://ems_app_dev',
-      changeOrigin: true,
-      secure: false,
-    },
-  },
-}
-})
+});
+
