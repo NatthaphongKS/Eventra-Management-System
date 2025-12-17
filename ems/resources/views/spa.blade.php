4469f4864c7a2ae58a400ebe4f74224323fa713d
NatthaphongKS <Natthaphong.KS1@gmail.com>
2025-12-17 08:59:06 +0700

    update icon in tab bar

--- ems/resources/views/spa.blade.php
+++ ems/resources/views/spa.blade.php
@@ -2,10 +2,12 @@
 <html lang="en">
 
 <head>
+    <link rel="icon" type="image/jpeg" href="{{ asset('images/clicknextIcon.png') }}?v=3">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Prompt:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Prompt:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
 
     <meta charset="UTF-8">
     @vite(['resources/js/app.js'])
@@ -20,5 +22,3 @@
 </body>
 
 </html>
-
-
