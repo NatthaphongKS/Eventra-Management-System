feb9a1c63ce4f761e38673c0e051ecf7dfb91385
NatthaphongKS <Natthaphong.KS1@gmail.com>
2025-12-17 16:42:21 +0700

    Update DepartmentSeeder.php

--- ems/database/seeders/DepartmentSeeder.php
+++ ems/database/seeders/DepartmentSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Seeders;
+dpm_namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,21 +13,21 @@ class DepartmentSeeder extends Seeder
     public function run(): void
     {
         $departments = [
-            ['id' => 1, 'name' => 'Artificial Intelligence'],
-            ['id' => 2, 'name' => 'Chatcone'],
-            ['id' => 3, 'name' => 'Developer'],
-            ['id' => 4, 'name' => 'Graphic'],
-            ['id' => 5, 'name' => 'IT Support'],
-            ['id' => 6, 'name' => 'Interactive Media'],
-            ['id' => 7, 'name' => 'MakeWebEasy'],
-            ['id' => 8, 'name' => 'Management'],
-            ['id' => 9, 'name' => 'Product Development'],
-            ['id' => 10, 'name' => 'SALE'],
-            ['id' => 11, 'name' => 'SMS'],
-            ['id' => 12, 'name' => 'Software Project'],
-            ['id' => 13, 'name' => 'System Engineer'],
-            ['id' => 14, 'name' => 'System Integration'],
-            ['id' => 15, 'name' => 'Byteforge Project'],
+            ['id' => 1, 'dpm_name' => 'Artificial Intelligence'],
+            ['id' => 2, 'dpm_name' => 'Chatcone'],
+            ['id' => 3, 'dpm_name' => 'Developer'],
+            ['id' => 4, 'dpm_name' => 'Graphic'],
+            ['id' => 5, 'dpm_name' => 'IT Support'],
+            ['id' => 6, 'dpm_name' => 'Interactive Media'],
+            ['id' => 7, 'dpm_name' => 'MakeWebEasy'],
+            ['id' => 8, 'dpm_name' => 'Management'],
+            ['id' => 9, 'dpm_name' => 'Product Development'],
+            ['id' => 10, 'dpm_name' => 'SALE'],
+            ['id' => 11, 'dpm_name' => 'SMS'],
+            ['id' => 12, 'dpm_name' => 'Software Project'],
+            ['id' => 13, 'dpm_name' => 'System Engineer'],
+            ['id' => 14, 'dpm_name' => 'System Integration'],
+            ['id' => 15, 'dpm_name' => 'Byteforge Project'],
         ];
 
         DB::table('ems_department')->insert($departments);
