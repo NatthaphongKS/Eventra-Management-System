740f0073aa06aaee6ba44c6f923162f2c4623807
NatthaphongKS <Natthaphong.KS1@gmail.com>
2025-12-17 16:02:22 +0700

    Fix
    fix seeder department/team/position

--- ems/database/seeders/DepartmentSeeder.php
+++ ems/database/seeders/DepartmentSeeder.php
@@ -12,26 +12,22 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        $departments = [
-            ['Product Development', 1],
-            ['Graphic', 2],
-            ['SMS', 3],
-            ['Management', 4],
-            ['Software Project', 5],
-            ['System Engineer', 6],
-            ['IT Support', 7],
-            ['System Integration', 8],
-            ['SALE', 9],
-            ['Artificial Intelligence', 10],
-            ['Interactive Media', 11],
-            ['MakeWebEasy', 12],
-            ['Chatcone', 13],
-            ['Developer', 14],
-            ['Team Leader', 15],
-            ['Development', 16],
-            ['Quality', 17],
-            ['Support', 18],
-            ['Planning', 19],
+       $departments = [
+            ['id' => 1, 'name' => 'Artificial Intelligence'],
+            ['id' => 2, 'name' => 'Chatcone'],
+            ['id' => 3, 'name' => 'Developer'],
+            ['id' => 4, 'name' => 'Graphic'],
+            ['id' => 5, 'name' => 'IT Support'],
+            ['id' => 6, 'name' => 'Interactive Media'],
+            ['id' => 7, 'name' => 'MakeWebEasy'],
+            ['id' => 8, 'name' => 'Management'],
+            ['id' => 9, 'name' => 'Product Development'],
+            ['id' => 10, 'name' => 'SALE'],
+            ['id' => 11, 'name' => 'SMS'],
+            ['id' => 12, 'name' => 'Software Project'],
+            ['id' => 13, 'name' => 'System Engineer'],
+            ['id' => 14, 'name' => 'System Integration'],
+            ['id' => 15, 'name' => 'Byteforge Project'],
         ];
 
         foreach ($departments as $name) {
