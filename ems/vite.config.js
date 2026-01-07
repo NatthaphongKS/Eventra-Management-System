ef862ec03235c57cd6caced87230f790eac3daaa
NatthaphongKS <Natthaphong.KS1@gmail.com>
2026-01-08 03:20:28 +0700

    Update vite.config.js

--- ems/vite.config.js
+++ ems/vite.config.js
@@ -1,21 +1,34 @@
-// vite.config.js
+
 // --------------------------------------------------------------------
-// Local
+// server
 // --------------------------------------------------------------------
 
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [
-    laravel({ input: ['resources/js/app.js'], refresh: true }),
-    vue(),
-  ],
-  server: {
-    host: '0.0.0.0',          // ให้ container เปิดพอร์ตออกมาได้
-    port: 5173,
-    strictPort: true,
-    hmr: { host: 'localhost', port: 5173 }, // ให้เบราว์เซอร์ยิงกลับ localhost
-  },
-})
+    plugins: [
+        laravel({ input: ["resources/js/app.js"], refresh: true }),
+        vue(),
+    ],
+    server: {
+        host: "0.0.0.0", // ให้ container เปิดพอร์ตออกมาได้
+        port: 7222,
+        strictPort: true,
+        hmr: { host: "10.80.7.17", port: 7222 }, // ให้เบราว์เซอร์ยิงกลับ localhost
+        proxy: {
+            "/api": {
+                target: "http://dekdee2.informatics.buu.ac.th:7111",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/sanctum": {
+                target: "http://dekdee2.informatics.buu.ac.th:7111",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+});
+
