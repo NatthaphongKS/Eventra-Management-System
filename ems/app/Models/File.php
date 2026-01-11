6fa704a95f5516c6a8f3be5cda565574b68f44f9
NatthaphongKS <Natthaphong.KS1@gmail.com>
2026-01-11 17:48:14 +0700

    Update File.php

--- ems/app/Models/File.php
+++ ems/app/Models/File.php
@@ -19,10 +19,6 @@ class File extends Model
         'file_path',
         'file_size',
         'file_type',
-        'file_upload_at',
-        'file_upload_by',
-        'file_delete_status',
-        'file_delete_by',
     ];
     protected $casts = [
         'file_upload_at' => 'datetime',
