146ebf516b7dc2727b24f5ea4d18e0e07631d85e
Raveroj <Raverojsonthi47@gmail.com>
2026-02-21 15:24:08 +0700

    ♻️ refactor EventService : เพิ่ม comment เปลี่ยนชื่อ function checkIn ตาม standard

--- ems/app/Service/EventServices/CheckInService.php
+++ ems/app/Service/EventServices/CheckInService.php
@@ -1,13 +1,24 @@
 <?php
 
+/**
+ * ชื่อไฟล์: CheckInService.php
+ * คำอธิบาย: ไฟล์นี้เก็บ (Business Logic) สำหรับจัดการข้อมูล Event ส่วน เช็คชื่อผู้เข้าร่วม
+ * ชื่อผู้เขียน/แก้ไข: รวีโรจน์ สนธิ
+ * วันที่จัดทำ/แก้ไข: 21 กุมภาพันธ์ 2569
+ */
+
+
 namespace App\Service\EventServices;
 
 use App\Models\Connect;
 use Illuminate\Support\Facades\DB;
 
+/* ============================================================
+       1) บันทึกคนเข้าร่วม
+    ============================================================ */
 class CheckInService
 {
-    public function checkin($eventId, $employeeId)
+    public function checkIn($eventId, $employeeId)
     {
         return DB::table('ems_connect')
             ->where('con_event_id', $eventId)
--- ems/app/Service/EventServices/ReplyFormService.php
+++ ems/app/Service/EventServices/ReplyFormService.php
@@ -1,10 +1,20 @@
 <?php
 
+/**
+ * ชื่อไฟล์: ReplyFormService.php
+ * คำอธิบาย: ไฟล์นี้เก็บ (Business Logic) สำหรับจัดการข้อมูล Event ส่วนการตอบกลับลิ้งเข้าร่วมกิจกรรม
+ * ชื่อผู้เขียน/แก้ไข: รวีโรจน์ สนธิ
+ * วันที่จัดทำ/แก้ไข: 21 กุมภาพันธ์ 2569
+ */
+
 namespace App\Service\EventServices;
 
 use App\Models\Connect;
 use Illuminate\Support\Facades\DB;
 
+ /* ============================================================
+       1) เก็บข้อมูลารตอบกลับลง DB
+    ============================================================ */
 class ReplyFormService
 {
     public function reply($eventId, $employeeId, $answer, $reason = null)
